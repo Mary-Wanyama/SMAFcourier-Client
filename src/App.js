07a5a5d80724bd20d8e891aba49acd093498f168
Felix Ondullah <felixondullah@gmail.com>
2022-12-04 19:39:04 +0300

    created components routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-// import { Routes ,Route } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
+import About from "./Components/About";
+import Admin from "./Components/Admin";
 
 
 const App =() => {
   return(
- <>
-<Home />
+    <>
+         <Routes>
+         <Route path='/' element={<Home/>} />
+         <Route path='/about' element={<About/>} />
+         <Route path='/admin' element={<Admin/>} />
+         </Routes>
+     
 </>
 
   )}
