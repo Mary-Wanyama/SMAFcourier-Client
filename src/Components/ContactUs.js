3ba931bd86cbdff9b2fb5ad455a8bdfd43f7cabf
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-12-11 17:30:52 +0300

    this commit styles the contactus.js file

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -3,11 +3,13 @@ import React, { Fragment } from "react"
 import "./ContactUs.css"
 import Email from "./email"
 
+
+
 const ContactUs =() =>{
     return(
         <Fragment>
          <div className="contactus">
-            <Email/>
+            <Email/> 
          </div> 
         </Fragment>
 
